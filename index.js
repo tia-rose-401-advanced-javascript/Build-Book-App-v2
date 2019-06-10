254777f2dbd936396c21e8854b8cf6f941cbefe7
Tia Rose <shortyshort789@aol.com>
2019-06-09 18:00:36 -0700

    added schema for sql and mongo. starting to feel sick so calling it a night

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 'use strict';
 
 require('dotenv').config();
-// const mongoose = require('mongoose');
-// const pg = require('pg');
-
-// let client = null;
-
-// if(process.env.DATABASE === 'pg'){
-//   client = new pg.Client(process.env.DATABASE_URL);
-//   client.connect();
-//   client.on('error', err => console.error(error));
-//   module.exports = client;
-// }else if(process.env.DATABASE === 'mongoose'){
-//   const mongooseOptions = {
-//     useNewURLParser:true,
-//     useCreateIndex: true,
-//   };
-
-//   mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
-// }
 
 require('./server').start(process.env.PORT);
